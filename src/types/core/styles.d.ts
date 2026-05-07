7adfd4f62140bb199f6c3cee5790bb699469a9f8
weiShao <1604705673@qq.com>
2026-05-07 11:24:12 +0800

    chore: 🔧暂存

---
+++ src/types/core/styles.d.ts
@@ -0,0 +1,14 @@
+/**
+ * 样式文件模块声明
+ * @description SCSS 和 CSS 文件的类型声明
+ */
+
+declare module '*.scss' {
+  const content: any
+  export default content
+}
+
+declare module '*.css' {
+  const content: any
+  export default content
+}
